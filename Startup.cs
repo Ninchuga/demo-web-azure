0ae1695db6e902253fa603651f4e09a3c08f3163
Ninoslav DJUKIC <ninoslav90@hotmail.com>
2021-12-22 16:30:03 +0100

    Update welcome message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace demo_web_azure
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!!!");
                 });
             });
         }
